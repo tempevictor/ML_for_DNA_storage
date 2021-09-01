919f25a9d5e33c2ae749546570e61ad9bf3d09d3
victor <victor.tempe@telecom-paristech.fr>
2021-09-01 18:24:32 +0200

    change pathss to modify

--- CATCaller_master/requirements/install_dynamicconv.sh
+++ CATCaller_master/requirements/install_dynamicconv.sh
@@ -6,13 +6,16 @@
 
 # note: replace /vol/bitbucket/vt520/ with apropriate path
 
+# **** CHANGE path to conda ****
 export PATH=/vol/bitbucket/vt520/miniconda3/bin/:$PATH
+#********************************
 source activate CATCaller
 source /vol/cuda/11.1.0-cudnn8.0.4.30/setup.sh
 TERM=vt100 # or TERM=xterm
 
-python /vol/bitbucket/vt520/CATCaller_master/dynamicconv_layer/cuda_function_gen.py
-python /vol/bitbucket/vt520/CATCaller_master/dynamicconv_layer/setup.py build
-python /vol/bitbucket/vt520/CATCaller_master/dynamicconv_layer/setup.py install --user
 
-
+# **** CHANGE path to CATCaller_master ****
+python /path/to/CATCaller_master/dynamicconv_layer/cuda_function_gen.py
+python /path/to/CATCaller_master/dynamicconv_layer/setup.py build
+python /path/to/CATCaller_master/dynamicconv_layer/setup.py install --user
+#********************************
